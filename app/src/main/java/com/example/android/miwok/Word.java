0b6fb1293b54b7e51e46df5ec43ec556474ebfd5
Jefferson Capovilla <jeffecap@motorola.com>
2017-12-22 17:09:53 -0200

    Lesson 4-13

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -1,17 +1,38 @@
 package com.example.android.miwok;
 
+import android.media.Image;
+import android.widget.ImageView;
+
 /**
  * Created by jeffecap on 18/07/17.
  */
 
 public class Word {
+
     private String mMiwokTranslation;
     private String mDefaultTranslation;
+    private int mImageResourceId;
 
-    //Constructor
+    /**
+     * Create a new Word object
+     * @param defaultTranslation is the word in language that the user is
+     * @param miwokTranslation is the word in the Miwok language
+     */
     public Word(String defaultTranslation, String miwokTranslation){
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
+    }
+
+    /**
+     *
+     * @param defaultTranslation
+     * @param miwokTranslation
+     * @param ImageResourceId is the drawable resource ID for the image assigned
+     */
+    public Word(String defaultTranslation, String miwokTranslation, int ImageResourceId){
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceId = ImageResourceId;
     }
 
     /**
@@ -28,6 +49,14 @@ public class Word {
      */
     public String getMiwokTranslation(){
         return mMiwokTranslation;
+    }
+
+    /**
+     * Return the image resource ID of the word
+     * @return
+     */
+    public int getImageResourceId() {
+        return mImageResourceId;
     }
 
 }
